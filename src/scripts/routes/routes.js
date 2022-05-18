347cee8270d0133689a94f7b20a60793132c0b3d
ramadanriz <zerorider96@gmail.com>
2022-05-18 17:46:33 +0700

    memperbaiki tampilan footer & membuat tampilan dasar about page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,7 +1,11 @@
 import Homepage from '../views/pages/homepage'
+import TodoPage from '../views/pages/todopage'
+import About from '../views/pages/about'
 
 const routes = {
-  '/': Homepage // default page
+  '/': Homepage, // default page
+  '/todo': TodoPage,
+  '/about': About
 }
 
 export default routes
---
+++ src/scripts/views/pages/todopage.js
@@ -0,0 +1,13 @@
+const TodoPage = {
+  async render () {
+    return `
+        <h2>Todo Page</h2>
+      `
+  },
+
+  async afterRender () {
+
+  }
+}
+
+export default TodoPage
