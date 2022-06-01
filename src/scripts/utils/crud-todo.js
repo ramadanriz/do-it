6f2368f9f5806e9dd73e1280e33ca4d7c53f1398
DwkLoki <dwiky.darmawansyah@gmail.com>
2022-06-01 17:10:05 +0700

    selesai membuat add todo feature

--- src/scripts/utils/crud-todo.js
+++ src/scripts/utils/crud-todo.js
@@ -1,9 +1,142 @@
 function crudTodo() {
+    const todos = [];
+
     const addTodoForm = document.getElementById("add-todo-form");
 
-    addTodoForm.addEventListener("submit", () => {
-        console.log("hello");
+    addTodoForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const todoId = +new Date();
+        const todoTitle = document.getElementById("todo-title-input").value;
+        
+        const generateTodoObject = () => {
+            return {
+                id: todoId,
+                task: todoTitle
+            }
+        }
+
+        const todoObject = generateTodoObject();
+
+        todos.push(todoObject);
+
+        //inisialisasi element container untuk todo
+        //dan mengosongkan isi element container sebelum looping data terbaru
+        const todoListContainer = document.querySelector(".todo-list-container");
+        todoListContainer.innerHTML = "";
+        
+        //looping semua data todo
+        for (const todoItem of todos) {
+            const newTodo = generateTodoElement(todoItem);
+            todoListContainer.append(newTodo);
+        }
+
+
+        document.getElementById("todo-title-input").value = null;
+
+        console.log(todos);
     })
+
+    // const generateTodoElement = (todoObject) => {
+    //     const checkBoxLabel = document.createElement("span");
+    //     checkBoxLabel.innerText = `&#10004;`;
+
+    //     const todoTitleElement = document.createElement("p");
+    //     todoTitleElement.classList.add("mb-0");
+    //     todoTitleElement.innerText = todoObject.task;
+
+    //     const labelElement = document.createElement("label");
+    //     labelElement.setAttribute("for", `task-${todoObject.id}`);
+    //     labelElement.append(checkBoxLabel, todoTitleElement);
+
+    //     const checkBoxElement = document.createElement("input");
+    //     checkBoxElement.setAttribute("class", "task-checkbox");
+    //     checkBoxElement.setAttribute("id", `task-${todoObject.id}`);
+    //     checkBoxElement.setAttribute("type", "checkbox");
+
+    //     const todoCardLeftSection = document.createElement("div");
+    //     todoCardLeftSection.setAttribute("class", "todo-card-left-section d-flex align-items-center w-75");
+    //     todoCardLeftSection.append(checkBoxElement, labelElement);
+
+    //     const editTodoIcon = document.createElement("ion-icon");
+    //     editTodoIcon.setAttribute("name", "create-outline");
+
+    //     const deleteTodoIcon = document.createElement("ion-icon");
+    //     deleteTodoIcon.setAttribute("name", "trash-outline");
+
+    //     const editTodoBtn = document.createElement("button");
+    //     editTodoBtn.setAttribute("class", "todo-card-edit-btn me-2 d-flex align-items-center");
+    //     editTodoBtn.append(editTodoIcon);
+
+    //     const deleteTodoBtn = document.createElement("button");
+    //     deleteTodoBtn.setAttribute("class", "todo-card-delete-btn d-flex align-items-center");
+    //     deleteTodoBtn.append(deleteTodoIcon);
+
+    //     const todoCardRightSection = document.createElement("div");
+    //     todoCardRightSection.setAttribute("class", "todo-card-right-section d-flex justify-content-end align-items-center w-25");
+    //     todoCardRightSection.append(editTodoBtn, deleteTodoBtn);
+
+    //     const todoCard = document.createElement("div");
+    //     todoCard.classList.add("todo-card", "p-3", "d-flex", "my-4");
+    //     todoCard.append(todoCardLeftSection, todoCardRightSection);
+
+    //     return todoCard;
+    // }
+
+    const generateTodoElement = (todoObject) => {
+        const todoCard = document.createElement("div");
+        todoCard.classList.add("todo-card", "p-3", "d-flex", "my-4");
+
+        todoCard.innerHTML = `
+            <div class="todo-card-left-section d-flex align-items-center w-75">
+                <input class="task-checkbox" id="task-${todoObject.id}" type="checkbox">
+                <label for="task-${todoObject.id}">
+                    <span>&#10004;</span>
+                    <p class="mb-0">${todoObject.task}</p>
+                </label>
+            </div>
+            <div class="todo-card-right-section d-flex justify-content-end align-items-center w-25">
+                <button class="todo-card-edit-btn me-2 d-flex align-items-center">
+                    <ion-icon name="create-outline"></ion-icon>
+                </button>
+                <button class="todo-card-delete-btn d-flex align-items-center">
+                    <ion-icon name="trash-outline" aria-label="delete"></ion-icon>
+                </button>
+            </div>
+        `
+        return todoCard;
+    }
+
+    function findTodoIndex(todoId) {
+        for(const index in todos){
+            if(todos[index].id === todoId){
+                return index
+            }
+        }
+
+        return -1
+    }
+
+    function removeTodo(todoId) {
+        const todoTarget = findTodoIndex(todoId);
+
+        if(todoTarget === -1){
+            return;
+        } 
+
+        todos.splice(todoTarget, 1);
+
+        //inisialisasi element container untuk todo
+        //dan mengosongkan isi element container sebelum looping data terbaru
+        const todoListContainer = document.querySelector(".todo-list-container");
+        todoListContainer.innerHTML = "";
+        
+        //looping semua data todo
+        for (const todoItem of todos) {
+            const newTodo = generateTodoElement(todoItem);
+            todoListContainer.append(newTodo);
+        }
+    }
 }
 
 export default crudTodo;
